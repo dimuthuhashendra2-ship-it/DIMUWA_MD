7d7be5fa3255ebc176fb8ad95fe0c552b883017a
dimuthuhashendra2-ship-it <dimuthuhashendra2@gmail.com>
2025-12-24 09:00:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // 🔑 Essential Session & Owner Settings
-    SESSION_ID: process.env.SESSION_ID || "Enter your session id",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "94743404814",
+    SESSION_ID: process.env.SESSION_ID || "MA82WbbI#yAA_FBSBDYe8gS1XH5Zwv2rSliKZhSJkpQW3ogOpIp4",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94742563284",
 
     // 🤖 Default Fallback Settings
-    DEFAULT_BOT_NAME: process.env.DEFAULT_BOT_NAME || "ZANTA-MD",
-    DEFAULT_OWNER_NAME: process.env.DEFAULT_OWNER_NAME || "Akash nimsara",
+    DEFAULT_BOT_NAME: process.env.DEFAULT_BOT_NAME || "DIMUWA-MD",
+    DEFAULT_OWNER_NAME: process.env.DEFAULT_OWNER_NAME || "DIMUWA HASHENDRA",
     DEFAULT_PREFIX: process.env.DEFAULT_PREFIX || ".",
 
     // 🖼️ Media Settings (Alive image & Menu)
@@ -21,10 +21,10 @@ module.exports = {
 
     // ⚙️ Dashboard / Database Defaults
     // මෙම අගයන් පළමු වතාවට Database එක සෑදීමේදී භාවිත වේ (Database & Dashboard Sync)
-    AUTO_READ: process.env.AUTO_READ || "false",
-    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    AUTO_READ: process.env.AUTO_READ || "true",
+    AUTO_TYPING: process.env.AUTO_TYPING || "true",
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     READ_CMD: process.env.READ_CMD || "false",
     AUTO_VOICE: process.env.AUTO_VOICE || "false",
     ANTI_BADWORD: process.env.ANTI_BADWORD || "false",
